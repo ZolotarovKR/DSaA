ad0a78eb8de40d4dd66969ecec4b632605de443c
Kiril Zolotarov <zolotarovkr@gmail.com>
2025-06-11 12:43:09 +0300

    refactor!(strings/z-function): rename functions

--- strings/z_function.py
+++ strings/z_function.py
@@ -1,12 +1,14 @@
-def build_z_function(s: str) -> list[int]:
+def z_function(s: str) -> list[int]:
     n = len(s)
-    z = [n]
+    z = [0]
     l, r = 0, 0
 
     for i in range(1, n):
         z.append(min(r - i, z[i - l]) if i < r else 0)
+
         while i + z[i] < n and s[z[i]] == s[i + z[i]]:
             z[-1] += 1
+
         if i + z[-1] > r:
             l = i
             r = i + z[-1]
@@ -14,9 +16,11 @@ def build_z_function(s: str) -> list[int]:
     return z
 
 
-def z_function(pat: str, txt: str):
+def find_matches(pat: str, txt: str):
+    m = len(pat)
+    n = len(txt)
     return (
         i
-        for i, length in enumerate(build_z_function(pat + "#" + txt))
-        if length >= len(pat)
+        for i, length in enumerate(z_function(pat + "#" + txt)[m + 1 : m + n - 1])
+        if length == m
     )
