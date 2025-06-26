09424fdd4ff32317fdb48174da5520fe6702c9a9
Kiril Zolotarov <zolotarovkr@gmail.com>
2025-06-27 00:27:19 +0300

    fix(flow/ford-fulkerson): visiting check in DFS

--- flow/ford_fulkerson.py
+++ flow/ford_fulkerson.py
@@ -2,9 +2,9 @@ from linked_list import LinkedList
 
 
 def find_max_flow(n: int, cap: list[list[int]], s: int, t: int) -> int:
-    visited = [False for _ in range(n)]
-
     def dfs() -> int:
+        visited = [False for _ in range(n)]
+        visited[s] = True
         stack = [(LinkedList[int].singleton(s), (1 << 32) - 1)]
 
         while stack:
@@ -19,15 +19,13 @@ def find_max_flow(n: int, cap: list[list[int]], s: int, t: int) -> int:
                     prev = curr
                 return flow
 
-            visited[u] = True
-
             for v in range(n):
                 if not visited[v] and cap[u][v] != 0:
+                    visited[v] = True
                     stack.append((path.cons(v), min(flow, cap[u][v])))
         return 0
 
     max_flow = 0
     while (f := dfs()) != 0:
         max_flow += f
-        visited = [False for _ in range(n)]
     return max_flow
